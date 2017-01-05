3be6fb3dd9a51bb94fd8daa587b08e6c6df0b513
Mehmet Top <mehmettop91@gmail.com>
2017-01-05 22:05:00 +0300

    Initial Commit

---
+++ AppiumTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppiumTestApp
+//
+//  Created by Mehmet Top on 05/01/2017.
+//  Copyright © 2017 Mehmet Top. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AppiumTestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppiumTestApp
+//
+//  Created by Mehmet Top on 05/01/2017.
+//  Copyright © 2017 Mehmet Top. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
